cbe2de45389202582ba75b280d7249018b5af2b5
Vagelis Goudouras <goudourasv@gmail.com>
2021-04-11 12:03:31 +0300

    Add istitution parameter to courses endpoint

--- src/main/java/com/goudourasv/courses/controller/CoursesResource.java
+++ src/main/java/com/goudourasv/courses/controller/CoursesResource.java
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.goudourasv.courses.domain.Course;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -16,27 +16,63 @@ import java.util.List;
 public class CoursesResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Course> getCourses() {
-        List<Course> coursesList = null;
+    //TODO Return courses matching an institution
+    public List<Course> getCourses(@QueryParam("institution") String institution) {
+        List<Course> coursesList = parseJsonFile();
+        
+        if (institution != null) {
+            List<Course> filteredList = getFilteredByInstitutionList(coursesList, institution);
+            return filteredList;
+        } else {
+            return coursesList;
+        }
+    }
+
+    private List<Course> parseJsonFile() {
+        List<Course> coursesList;
         ObjectMapper mapper = new ObjectMapper();
         try {
             String file = Thread.currentThread().getContextClassLoader().getResource("/courses.json").getFile();
             coursesList = mapper.readValue(new File(file), new TypeReference<List<Course>>() {
             });
         } catch (Exception ex) {
-            //TODO return 500 error
-            return null;
+            throw new ServerErrorException("Something went wrong", Response.Status.INTERNAL_SERVER_ERROR);
         }
         return coursesList;
     }
 
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Course getCourse(){
-
+    private List<Course> getFilteredByInstitutionList(List<Course> coursesList, String institution) {
+        List<Course> filteredByInstitutionList = new ArrayList<Course>();
+        for (Course course : coursesList) {
+            if (course.getInstitutionName().equals(institution)) {
+                filteredByInstitutionList.add(course);
+            }
+        }
+        return filteredByInstitutionList;
     }
 
-}
+//    //TODO Take from the same file a specific course from the list
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Course getCourse(@QueryParam("id") int id) {
+//        Course myCourse = new Course();
+//        List<Course> coursesList;
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            String file = Thread.currentThread().getContextClassLoader().getResource("/courses.json").getFile();
+//            coursesList = mapper.readValue(new File(file), new TypeReference<List<Course>>() {
+//            });
+//        } catch (Exception ex) {
+//            throw new ServerErrorException("Something went wrong", Response.Status.INTERNAL_SERVER_ERROR);
+//        }
+//
+//        for (Course course : coursesList) {
+//            if (course.getId()==id) {
+//                myCourse = course;
+//            }
+//        }
+//        return myCourse;
+//    }
 
+}
 
